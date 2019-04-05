021d307cd4a19b12eb314d574528956ef6c82007
Elvira <elvira.kalamina@mail.ru>
2019-04-05 22:02:30 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Signin.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Signup from './Signup'
 import Signin from './Signin'
@@ -11,19 +11,40 @@ import ShowPost from './ShowPost'
 import { Switch, Route} from "react-router";
 
 
-class App extends React.Component {
-    render(){
-    return(<ShowPost/>)
-    }
-    }
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <button><Link to='/'>Home</Link></button>
+        <button><Link to='/signin'>Sign In</Link></button>
+        <button><Link to='/signup'>Sign Up</Link></button>
+        <button><Link to='/addpost'>Add Post</Link></button>
+      </ul>
+    </nav>
+  </header>
+)
+const Main = () => (
+  <main>
+     <Switch>
+       <Route exact path='/' component = {ShowPost}/>
+       <Route path='/signin' component = {Signin}/>
+       <Route path='/signup' component = {Signup}/>
+       <Route path='/addpost' component = {AddPost}/>
+     </Switch>
+  </main>
+)
+
+
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+)
 
     ReactDOM.render(
-          /*  <BrowserRouter>
-                <Switch>
-                    <Route exact path='/' component = {ShowPost}/>
-                    <Route path='/signin' component = {Signin}/>
-                    <Route path='/signup' component = {Signup}/>
-                    <Route path='/addpost' component = {AddPost}/>
-                </Switch>
-            </BrowserRouter>*/ <App/>
+          <BrowserRouter>
+            <App/>
+          </BrowserRouter>
     , document.getElementById('root'));
